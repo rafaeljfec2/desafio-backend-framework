2a4978fa50c6e48d428ca2562c1f1121566eb8a5
Rafael de Jesus Ferreira <rafaeljfec2@gmail.com>
2021-10-22 17:21:49 -0300

    add RF1 trello

--- src/usecases/CreateShopkeeper/CreateShopkeeperUserCase.ts
+++ src/usecases/CreateShopkeeper/CreateShopkeeperUserCase.ts
@@ -1,5 +1,7 @@
+import { IAccount } from '@modules/entities/Account/IAccount';
 import { IAccountRepository } from '@modules/entities/Account/IAccountRepository';
 import { ICreateAccountDTO } from '@modules/entities/Account/ICreateAccountDTO';
+import AppError from '@modules/shared/errors/AppError';
 
 export class CreateShopkeeperUseCase {
   constructor(private accountRepository: IAccountRepository) {}
@@ -10,7 +12,21 @@ export class CreateShopkeeperUseCase {
     email,
     password,
     type,
-  }: ICreateAccountDTO): Promise<void> {
+  }: ICreateAccountDTO): Promise<IAccount> {
+    if (await this.accountRepository.findByDocument(document)) {
+      throw new AppError(
+        'There is already one shopkeeper with this document',
+        422,
+      );
+    }
+
+    if (await this.accountRepository.findByEmail(email)) {
+      throw new AppError(
+        'There is already one shopkeeper with this email',
+        422,
+      );
+    }
+
     const shopkeeper = await this.accountRepository.create({
       name,
       document,
@@ -19,6 +35,6 @@ export class CreateShopkeeperUseCase {
       type,
     });
 
-    await this.accountRepository.save(shopkeeper);
+    return shopkeeper;
   }
 }
--- src/usecases/CreateUser/CreateUserUseCase.ts
+++ src/usecases/CreateUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { IAccount } from '@modules/entities/Account/IAccount';
 import { IAccountRepository } from '@modules/entities/Account/IAccountRepository';
 import { ICreateAccountDTO } from '@modules/entities/Account/ICreateAccountDTO';
+import AppError from '@modules/shared/errors/AppError';
 
 export class CreateUserUseCase {
   constructor(private accountRepository: IAccountRepository) {}
@@ -12,6 +13,14 @@ export class CreateUserUseCase {
     password,
     type,
   }: ICreateAccountDTO): Promise<IAccount> {
+    if (await this.accountRepository.findByDocument(document)) {
+      throw new AppError('There is already one user with this document', 422);
+    }
+
+    if (await this.accountRepository.findByEmail(email)) {
+      throw new AppError('There is already one user with this email', 422);
+    }
+
     const user = await this.accountRepository.create({
       name,
       document,
@@ -19,8 +28,6 @@ export class CreateUserUseCase {
       password,
       type,
     });
-
-    await this.accountRepository.save(user);
 
     return user;
   }
