de7582c858a021708d45e5f7de2b795e1068fa92
Rafael de Jesus Ferreira <rafaeljfec2@gmail.com>
2021-10-23 15:06:30 -0300

    add test unit credit

---
+++ src/usecases/CreateCreditAccount/CreateCreditAccount.spec.ts
@@ -0,0 +1,51 @@
+import { ICreateMovementDTO } from '@modules/entities/Movement/ICreateMovementDTO';
+import Crypt from '@modules/infraestructure/auth/Crypt';
+import MemoryAccountRepository from '@modules/infraestructure/repositories/orm/memory/MemoryAccountRepository';
+import MemoryMovementRepository from '@modules/infraestructure/repositories/orm/memory/MemoryMovementRepository';
+import 'reflect-metadata';
+import { BalanceAccountUseCase } from '../BalanceAccount/BalanceAccountUseCase';
+import CreateCreditAccountUseCase from './CreateCreditAccountUseCase';
+import { CreateUserUseCase } from '../CreateUser/CreateUserUseCase';
+
+let memoryMovementRepository: MemoryMovementRepository;
+let memoryAccountRepository: MemoryAccountRepository;
+let createCreditAccounttUseCase: CreateCreditAccountUseCase;
+let createUseUserCase: CreateUserUseCase;
+let balanceAccountUseCase: BalanceAccountUseCase;
+
+describe('Credit a Account', () => {
+  beforeEach(() => {
+    memoryMovementRepository = new MemoryMovementRepository();
+    memoryAccountRepository = new MemoryAccountRepository();
+    createCreditAccounttUseCase = new CreateCreditAccountUseCase(
+      memoryMovementRepository,
+      memoryAccountRepository,
+    );
+
+    createUseUserCase = new CreateUserUseCase(
+      memoryAccountRepository,
+      new Crypt(),
+    );
+    balanceAccountUseCase = new BalanceAccountUseCase(
+      memoryAccountRepository,
+      memoryMovementRepository,
+    );
+  });
+
+  it('Should be able to credit origin account.', async () => {
+    const account = await createUseUserCase.execute({
+      name: 'Rafael',
+      document: '09181018673',
+      email: 'rafaeljfec@gmail.com',
+      password: '123',
+      type: 'USER',
+    });
+
+    const createCredit: ICreateMovementDTO = { account: account, value: 1 };
+
+    await createCreditAccounttUseCase.execute(createCredit);
+    const balance = await balanceAccountUseCase.execute(account.document);
+
+    expect(balance).toEqual(1);
+  });
+});
